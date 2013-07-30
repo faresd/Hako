a3b4fa6d52d3df812acc8957e2060684da281e0f
Fares DROUBI <fdr@zenexity.com>
2013-07-30 11:52:20 +0200

    security fix

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,21 @@
 package controllers
-
+import com.typesafe.plugin._
+import play.api.Play.current
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
   
   def index = Action {
+  	val mail = use[MailerPlugin].email
+	mail.setSubject("mailer")
+	mail.addRecipient("fares.droubi@gmail.com")
+	mail.addFrom("fdr@zenexity.com")
+	//sends text/text
+	mail.send( "textf safaoifjdoiajfdsoijsf" )
+
     Ok(views.html.index("Your new application is ready."))
   }
+
   
 }
